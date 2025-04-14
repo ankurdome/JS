6a65255ccb48b9c080e5d3476483c48aacbced8e
ankurdome <ankurdome02@gmail.com>
2025-04-14 23:25:41 +0530

    from the vs code

---
+++ variables.js
@@ -0,0 +1,16 @@
+const accountID = 4556;   //constant value declaration
+let accountEmail = "ankurdome@gmail.com";  //use let 
+var accountPassword = "12345";   //now we dont use var cause there wasn't scope {}
+accountCity = "Jaipur";   //vaiablle
+
+// accountID = 2;
+console.log(accountID);
+
+accountEmail = "ankur@gmaial.com";
+accountPassword = "23456";
+accountCity = "Pune";
+
+let accountState; //no value assigned so its undefind type
+
+console.log([accountEmail,accountID,accountPassword,accountCity,accountState]);
+
