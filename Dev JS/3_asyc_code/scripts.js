ccda8763698e0eb31f6cc56e096c0021fc489c8e
ankurdome <ankurdome02@gmail.com>
2025-08-23 22:27:08 +0530

    dom+events done

---
+++ Dev JS/3_asyc_code/scripts.js
@@ -0,0 +1,20 @@
+/*
+JAVASCRIPT (default)
+- JavaScript is synchronous by default (one code at a time)
+- single threaded 
+
+Execution Context:
+- each operation waits for last one to complete before one
+
+
+>> BLOCKING Code vs NON-BLOCKING <<
+(both are required as per use case)
+
+Blocking: blocks the flow of program
+eg: file read sync
+
+Non-BLocking: does not block executiom
+eg: read file async
+
+
+*/
